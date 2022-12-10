636f3b6e6e53c9722d4f86373c4f7951320a1019
Randy Groleau <groleaurandy@yahoo.com>
2022-12-10 12:41:23 -0500

    Add Regions to FormatTabClickEvent Tab

--- Notepad+/ByTab/FormatTab/Click Events/FormatTabClickEvents.cs
+++ Notepad+/ByTab/FormatTab/Click Events/FormatTabClickEvents.cs
@@ -5,25 +5,40 @@ namespace NotepadPlus
 
     public partial class Form1 : Form
     {
+
+        #region ChangeFont
+
         //Change Font
         private void changeSettingsToolStripMenuItem1_Click(object sender, EventArgs e)
         {
             GetNotePadArea.Font = _fontOptionsUtil.ChangeFont();
         }
 
-        //Save Settings
-        private void saveSettingsToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            _fontOptionsUtil.SaveSettings();
-        }
-       
-
         //Restore Settings
         private void restoreSettingsToolStripMenuItem_Click(object sender, EventArgs e)
         {
             GetNotePadArea.Font = _fontOptionsUtil.Restore();
         }
-   
+
+
+        #endregion
+
+        #region SaveSettings
+
+        //Save Settings
+        private void saveSettingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _fontOptionsUtil.SaveSettings();
+        }
+
+        #endregion
+
+
+
+
+
+
+
 
     }
 }
